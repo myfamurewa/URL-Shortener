0582586395b691f583b8108b437d0a1419481113
Michael Famurewa <myfamurewa@gmail.com>
2020-10-29 11:49:13 -0400

    almost done

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk')
+const { nanoid } = require('nanoid');
+
+require('dotenv').config()
+
+const db = monk(process.env.MONGODB_URI)
+const url = db.get('urls')
+urls.createIndex({slug: 1}, {unique: true})
+
+
+const app = express()
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json())
+app.use(express.static('./public'))
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+    
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'myfams Url shortener'
+    })
+});
+
+app.post('/url', async (req, res, next) => {
+    let {slug, url} = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url,
+        })
+        if (!slug) {
+            slug = nanoid(7);
+        } else {
+            const existing = await urls.findOne({slug})
+            if (existing) {
+                throw new Error('slug is already being used. 😒')
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        res.json({
+            slug,
+            url,
+        })
+        const created = await urls.insert(newUrl)
+        res.json(created)
+    } catch (error) {
+        next(error)
+    }
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status)
+    } else {
+        res.status(500)
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === "production" ? "🍞" : error.stack
+    })
+});
+
+app.get('/:id', async (req, res) => {
+    const {id: slug} = req.params;
+    try {
+        const url = await urls.findOne({ slug });
+        if (url) {
+            res.redirect(url.url)
+        }
+        res.redirect(`/?error=${slug} not found`)
+    } catch (error) {
+        res.redirect(`/?error=Link not found`)
+    }
+});
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,25 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: '',
+        slug: '',
+        created: null,
+    },
+    methods: {
+        async createUrl() {
+            console.log(this.url, this.slug);
+            const response = await fetch('/url', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    url: this.url,
+                    slug: this.slug
+                })
+            });
+            this.crated = await response.json()
+        }
+    }
+})
